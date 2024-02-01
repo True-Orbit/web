53fecf5e04044ab724dc2f98f8cf2cbfa15fbbc8
kevin townsend <kevintownsend@kevins-MacBook-Pro.local>
2024-01-31 19:44:32 -0800

    cleanup

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,23 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { UserModel } from '@/resources/user';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 type ResponseData = {
   data: UserModel[];
-};
-
-const fakeUser: UserModel = {
-  id: '1',
-  email: 'fakeUser@example.com',
-  firstName: 'Leanne',
-  lastName: 'Graham',
-  profileUrl: 'some-url',
-  handle: 'Gripes',
-  avatar: 'some-avatar',
-  createdAt: new Date(),
-  updatedAt: new Date(),
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
