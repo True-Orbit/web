6f9d32c8aee62318d046ed3d99f63a314f4bf9e5
Kevin <soccerkevint11@gmail.com>
2024-12-29 21:55:39 -0800

    add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,16 @@
-const About = () => {
-  return <h1>Welcome to Smurf Social!</h1>;
-};
+import { Container } from '@mui/material';
+
+const About = () => (
+  <Container>
+    <h1>Welcome to True Orbit</h1>
+    <h2>Our Mission</h2>
+    <p>
+      It has become undeniably apparent that most of the media, especially social media has abandoned truth for power.
+      Our mission is to create a social platform of honest, thoughtful dialogue. We believe free expression should not
+      come at the cost of truth. That's why we have built our community guidelines and features around one core
+      principle: safeguarding authenticity and we actively take measures to minimize the spread of misinformation.
+    </p>
+  </Container>
+);
 
 export default About;
