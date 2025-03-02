5d005843857007402f061dfd2735cdf1a659009e
Kevin <soccerkevint11@gmail.com>
2025-03-01 17:02:05 -0800

    add provider list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
-'use client'; // This is a client component
+'use client';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { SessionProvider } from 'next-auth/react';
 
 import { createTheme } from '@/app/styles/themes';
+
+import { AuthProvider } from '@/resources/auth';
+import { ProviderList } from '@/components/basic';
 
 import './styles/reset.css';
 import './styles/globals.scss';
@@ -18,13 +21,16 @@ interface RootLayoutProps {
 export default function RootLayout({ children, session }: RootLayoutProps) {
   const theme = createTheme();
 
+  const providers = [AuthProvider, CssBaseline];
+
   return (
     <SessionProvider session={session}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <html lang="en">
-          <body>{children}</body>
-        </html>
+        <ProviderList providers={providers}>
+          <html lang="en">
+            <body>{children}</body>
+          </html>
+        </ProviderList>
       </ThemeProvider>
     </SessionProvider>
   );
