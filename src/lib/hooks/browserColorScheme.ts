d10526cfc06a8a516fa6413ba5be4f5a7fcea1b6
Kevin <soccerkevint11@gmail.com>
2025-03-03 22:22:20 -0800

    add turbopack

--- src/lib/hooks/browserColorScheme.ts
+++ src/lib/hooks/browserColorScheme.ts
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 
 export const useBrowserColorScheme = (): 'light' | 'dark' => {
-  const mediaQuery = window.matchMedia('(prefers-color-scheme: light)');
-  const [isLightMode, setIsLightMode] = useState(mediaQuery.matches);
+  const [isLightMode, setIsLightMode] = useState(true);
   
   useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: light)');
+    setIsLightMode(mediaQuery.matches);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleChange = (e: any) => {
       if (isLightMode !== e.matches) setIsLightMode(e.matches);
