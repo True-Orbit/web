76d154c5213abe41de017406722826f158bc979c
Kevin <soccerkevint11@gmail.com>
2025-03-05 23:29:37 -0800

    add interceptor to axios client

--- src/lib/utils/apiClient.ts
+++ src/lib/utils/apiClient.ts
@@ -3,10 +3,34 @@ import axios from 'axios';
 const BASE_URL = process.env.DOMAIN!;
 
 const apiClient = axios.create({
+  withCredentials: true,
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+apiClient.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+    if (
+      error.response 
+      && error.response.status === 401 
+      && !originalRequest._retry
+      && !error.response.request.responseURL.includes("/auth/refresh")
+    ) {
+      originalRequest._retry = true;
+      try {
+        await apiClient.post('/auth/refresh');
+
+        return apiClient(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
