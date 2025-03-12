cf1a032e0b0d7a3c6a9a07b4fb8768778bdb4dec
Kevin <soccerkevint11@gmail.com>
2025-03-11 18:47:36 -0700

    update secrets unpacking

--- scripts/unpackSecrets.sh
+++ scripts/unpackSecrets.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # entrypoint.sh
 
+if [ -n "$ORG_SECRETS" ]; then
+  echo "Parsing ORG_SECRETS and exporting variables..."
+  eval "$(
+    echo "$ORG_SECRETS" \
+      | jq -r 'to_entries | map("export " + .key + "=" + (.value|@sh)) | .[]'
+  )"
+fi
+
 if [ -n "$WEB_SERVICE_SECRETS" ]; then
   echo "Parsing WEB_SERVICE_SECRETS and exporting variables..."
   eval "$(
